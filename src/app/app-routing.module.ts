982a0acc637f7f0579b3a01694987aa95d7b8739
abwonji95 <abwonji95@gmail.com>
2022-06-20 21:43:42 +0300

    set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LoginpageComponent } from './pages/loginpage/loginpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'loginpage',component:LoginpageComponent},
+  {path:'homepage',component:HomepageComponent},
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/loginpage/loginpage.component.ts
+++ src/app/pages/loginpage/loginpage.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginpage',
@@ -6,10 +16,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loginpage.component.css']
 })
 export class LoginpageComponent implements OnInit {
+  matcher = new MyErrorStateMatcher();
+  form = new FormGroup({
+    emailFormControl: new FormControl('', [Validators.required]),
+    passwordFormControl: new FormControl('', [Validators.required]),
+  });
+  email= new FormControl('', [Validators.required, Validators.email])
+  password= new FormControl('', [Validators.required])
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
 
-  ngOnInit(): void {
+  ) 
+  {
+    this.form = this.formBuilder.group({
+      email: [''],
+      password: [''],
+    });
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
+  ngOnInit() {}
+  loginUser(){}
 
+}
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
 }
