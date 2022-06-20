7c96dd610f2ddcdb1778236e3eeae1316cb4dc72
abwonji95 <abwonji95@gmail.com>
2022-06-20 21:31:27 +0300

    set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { LoginpageComponent } from './pages/loginpage/loginpage.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AngularMaterialModule } from './pages/modules/material-module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    LoginpageComponent
+    LoginpageComponent,
+    NavBarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+
+imports: [
+  BrowserModule,
+  AppRoutingModule,
+  AngularMaterialModule,
+  FormsModule,
+  ReactiveFormsModule,
+  MatNativeDateModule,
+  HttpClientModule,
+  BrowserAnimationsModule
+],
+providers: [
+  DatePipe
+],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
